e5795f17ebf3dddbd801bdff49e1add5b78a7ff3
FadyBoj <fadynabil701@gmail.com>
2023-11-04 10:42:36 +0200

    implemented httpOnly cookies for user-Authentication + logout

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,8 @@
 const User = require('../models/User');
 const CustomAPIError = require('../error/CustomAPIError');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+//Create Account
 
 const createAccount = async(req,res) =>{
     const {firstname, lastname, email, password, age} = req.body;
@@ -31,7 +34,74 @@ const createAccount = async(req,res) =>{
     }
 }
 
+// Login
+
+const login = async(req,res) =>{
+
+    const { email, password } = req.body
+
+    if(!email || !password)
+    {
+        throw new CustomAPIError("Please provide email and password",400)
+    }
+
+        const user = await User.find({email:email.toLowerCase()});
+        if(user.length > 0 && user[0].password === password)
+        {
+            const data = {
+                id:user[0]._id,
+                firstname:user[0].firstname,
+                lastname:user[0].lastname,
+                email:user[0].email,
+                address:user[0].address,
+                cartItems:user[0].cartItems,
+                previousOrders:user[0].previousOrders,
+                wishList:user[0].wishList
+            }
+
+            const oneDay = 1000 * 60 * 60 * 24;
+            const token = jwt.sign(data,process.env.JWT_SECRET,{expiresIn:'1d'});
+            res.cookie('jwtToken',token,{httpOnly:true,secre:true,maxAge:oneDay})
+            res.status(200).json({msg:"Successfully logged in !"})
+        }
+        else{
+            throw new CustomAPIError("Username and password are not matched ",400)
+        }
+        
+
+}
+
+// check Auth
+
+const checkAuth = async(req,res) =>{
+    const token = req.cookies.jwtToken;
+
+    if(!token)
+    throw new CustomAPIError("Not authorized",401);
+
+    try {
+        const decodedToken = jwt.verify(token,process.env.JWT_SECRET);
+        res.status(200).json({user:decodedToken});
+    } catch (error) {
+        console.log("Error !")
+        throw new CustomAPIError("Invalid Token",498);
+    }
+}
+
+const logout = async(req,res) =>{
+    const token = req.cookies.jwtToken;
+
+    if(!token)
+    throw new CustomAPIError("No token provided",404);
+
+    res.clearCookie('jwtToken');
+    res.status(200).json({msg:'Logged out'});
+}
+
 
 module.exports = {
-    createAccount
+    createAccount,
+    login,
+    checkAuth,
+    logout
 }
--- routes/users-route.js
+++ routes/users-route.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 const {
-    createAccount
+    createAccount,
+    login,
+    checkAuth,
+    logout
 } = require('../controllers/users');
 
 router.route('/register').post(createAccount);
+router.route('/login').post(login);
+router.route('/check-auth').get(checkAuth);
+router.route('/logout').get(logout)
+
 
 
 module.exports = router

5650ace6c1e64d0e94f190403bf854d242f26e7a
FadyBoj <fadynabil701@gmail.com>
2023-11-05 15:03:22 +0200

    Now passwords are being encrypted before it gets stored in the DB

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,12 @@
 const User = require('../models/User');
 const CustomAPIError = require('../error/CustomAPIError');
 const jwt = require('jsonwebtoken');
-require('dotenv').config()
+const bcrypt = require('bcrypt');
+require('dotenv').config();
 //Create Account
 
 const createAccount = async(req,res) =>{
     const {firstname, lastname, email, password, age} = req.body;
-    console.log(req.body)
 
     if(!firstname || !lastname || !email || !password || !age)
     throw new CustomAPIError('Please fill in your information',400);
@@ -19,19 +19,25 @@ const createAccount = async(req,res) =>{
         throw new CustomAPIError('This email is registered before, please try to login to your account',400);
     }
 
-    try {
-        await User.create({
-            firstname:firstname,
-            lastname:lastname,
-            email:email.toLowerCase(),
-            password:password,
-            age:age
-        })
-        console.log("Successfully created a new account");
-        res.status(200).json({msg:"successfully created your account"})
-    } catch (error) {
-        console.log(error)
-    }
+    bcrypt.hash(password,10,async(err,hash) =>{
+
+        try {
+            await User.create({
+                firstname:firstname,
+                lastname:lastname,
+                email:email.toLowerCase(),
+                password:hash,
+                age:age
+            })
+            res.status(200).json({msg:"successfully created your account"})
+        } catch (error) {
+            console.log(error)
+        }
+
+    })
+
+
+    
 }
 
 // Login
@@ -46,27 +52,37 @@ const login = async(req,res) =>{
     }
 
         const user = await User.find({email:email.toLowerCase()});
-        if(user.length > 0 && user[0].password === password)
+
+        if(user.length === 0)
         {
-            const data = {
+            throw new CustomAPIError("This account is not registered yet, please try to create a new account",404)
+        }
+        else{
+            bcrypt.compare(password,user[0].password,(err,result) =>{
+               if(!result)
+               return res.status(401).json({msg:"Email and password are not matched"});
+
+               const data = {
                 id:user[0]._id,
+                email:user[0].email,
                 firstname:user[0].firstname,
                 lastname:user[0].lastname,
-                email:user[0].email,
                 address:user[0].address,
                 cartItems:user[0].cartItems,
                 previousOrders:user[0].previousOrders,
                 wishList:user[0].wishList
-            }
+               }
 
-            const oneDay = 1000 * 60 * 60 * 24;
-            const token = jwt.sign(data,process.env.JWT_SECRET,{expiresIn:'1d'});
-            res.cookie('jwtToken',token,{httpOnly:true,secre:true,maxAge:oneDay})
-            res.status(200).json({msg:"Successfully logged in !"})
+               const oneDay = 1000 * 60 * 60 * 24;
+               const token = jwt.sign(data,process.env.JWT_SECRET,{expiresIn:'1d'});
+               res.cookie('jwtToken',token,{httpOnly:true,secure:true,maxAge:oneDay});
+               res.status(200).json({msg:"Successfully logged in"});
+
+            })
         }
-        else{
-            throw new CustomAPIError("Username and password are not matched ",400)
-        }
+
+
+        
         
 
 }

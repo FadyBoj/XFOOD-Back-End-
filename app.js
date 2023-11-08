adc44fdd4a8ca4e5cf32762c60adbc58199e02f9
FadyBoj <fadynabil701@gmail.com>
2023-11-08 20:37:52 +0200

    Optimized for deploying

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const path = require('path')
 const cookieParser = require('cookie-parser');
 const connectDB = require('./db/connect');
 require('dotenv').config();
@@ -10,6 +11,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler-middleware');
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static('./dist'))
 
 //routes
 const usersRoute = require('./routes/users-route');
@@ -21,7 +23,7 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res) =>{
-    res.send("Test")
+    res.sendFile(path.resolve('./view/index.html'))
 })
 
 const start = async() =>{

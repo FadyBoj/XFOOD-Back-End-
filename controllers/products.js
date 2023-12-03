4ee53c68e53f577ee8ed98055494da73f1d164c5
FadyBoj <fadynabil701@gmail.com>
2023-12-03 19:58:29 +0200

    now products can be filtered using url queries

--- controllers/products.js
+++ controllers/products.js
@@ -2,9 +2,14 @@ const Product = require('../models/Product');
 const CustomAPIError= require('../error/CustomAPIError');
 
 const getProducts = async(req,res) =>{
+    const { category } = req.query
+    const queryObject = {}
+    
+    if(category)
+    queryObject.category = category;
 
     try {
-        const products = await Product.find({});
+        const products = await Product.find(queryObject);
         res.status(200).json(products)
         
     } catch (error) {
